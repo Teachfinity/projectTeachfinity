80329de853bf42bd604d418fab6d67fd5fe5b6d4
Ali Zaib <alizaib2706@gmail.com>
2020-12-17 11:44:27 +0500

    timetable-file-in process

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,41 +1,90 @@
 import React, { Component } from "react" ;
 import {
   MuiPickersUtilsProvider,
-  KeyboardDatePicker
+  KeyboardDatePicker,
+  KeyboardTimePicker
 } from '@material-ui/pickers';
 import 'date-fns';
+import { Calendar, Views  , momentLocalizer}  from 'react-big-calendar' ;
+
+import events from "../assets/events"
+import moment from 'moment'
 import DateFnsUtils from '@date-io/date-fns';
 import "../App.css"
+
+const propTypes = {}
+
 class Timetable extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedDate: new Date() 
-    }
+  constructor(...args){
+    super(...args);
+    this.state = {events}
 
   }
+
    handleDateChange = (date) => {
+     
     console.log(date);
-    this.setState({setSelectedDate: date});
-    
-  };
+    this.setState({selectedDate: date}) ;
+    }; 
+
+
+    handleSelect = ({ start, end }) => {
+      const title = window.prompt('New Event name')
+      if (title)
+        this.setState({
+          events: [
+            ...this.state.events,
+            {
+              start,
+              end,
+              title,
+            },
+          ],
+        })
+    }
   render(){
+    const localizer= momentLocalizer(moment);
   return (
-      <div >
+   
+    <div >
+     <div  >
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
 
         <KeyboardDatePicker
-          label="Material Date Picker"
+        id="time-picker"
+          label="Time Picker"
+          
           value={this.state.selectedDate}
           onChange={this.handleDateChange}
-          onClose={() => alert(this.state.selectedDate)}
+          
+        />
+        <KeyboardTimePicker
+        id="time-picker"
+          label="Time Picker"
+          
+          value={this.state.selectedDate}
+          onChange={this.handleDateChange}
+          
+        />
+      </MuiPickersUtilsProvider>
+</div> 
+<Calendar
+          selectable
+          localizer={localizer}
+          events={this.state.events}
+          defaultView={Views.month}
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2015, 3, 12)}
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={this.handleSelect}
         />
 
-      </MuiPickersUtilsProvider>
-        
-      </div>
+
+
+
+</div>
   );
   }
 }
-
+Timetable.propTypes = propTypes
 export default Timetable;
