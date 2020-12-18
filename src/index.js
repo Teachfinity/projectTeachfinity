c0134e66cdb7d073697d78595022a6667d413df5
Mahno-or <mahnoorsaleem.comsats@gmail.com>
2020-12-18 18:23:56 +0500

    Timetable

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css' ;
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "react-big-calendar/lib/css/react-big-calendar.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
