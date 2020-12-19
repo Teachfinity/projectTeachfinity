468a2f4b71779066ed9a3a8fbe393701221ec27d
Mahno-or <mahnoorsaleem.comsats@gmail.com>
2020-12-19 12:36:09 +0500

    diary timetable

--- src/components/Diary.js
+++ src/components/Diary.js
@@ -9,32 +9,45 @@ class Diary extends Component {
     this.state = {
       input: '',
       task: [],
-      button: 'Add'
+      button: 'Add',
+      key: ''
     }
   }
   updatestate = event => {
-    const { value } = event.target;
+    const {value}  = event.target;
     this.setState({ input: value })
   }
   addtask = () => {
     if (this.state.button === 'Update') {
-      this.setState({ task: [this.state.input] })
-      this.setState({ input: '' })
-      this.setState({ button: 'Add' })
+      const t = this.state.task
+      const update = t.map(taski=>{
+        if(taski===this.state.key){
+          return taski = this.state.input
+        }
+      })
+      this.setState({task: [...this.state.task, update]})
+      this.setState({button: "Add"})
+      this.setState({input: ""})
     }
     else if (this.state.input !== '') {
       this.setState({ task: [...this.state.task, this.state.input] })
       this.setState({ input: '' })
     }
   }
+  removetask = (taskkey) =>{
+    const remove = this.state.task.filter(taskd =>{
+      return taskd!==taskkey
+    });
+    this.setState({task: [...remove]})
+  }
   render() {
     return (
-      <header className="background">
+      <header className="bg">
         <header className="background-cover">
-          <div className="container">
+          <div className="box">
             <br></br><h1>My Diary</h1><br></br>
             <div>
-              <TextField className="textfield" defaultValue={this.state.input} onChange={this.updatestate} label="Todo" variant="outlined" placeholder="add task..."></TextField>
+              <TextField className="textfield" value={this.state.input} onChange={this.updatestate} label="Todo" variant="outlined" placeholder="add task..."></TextField>
               <Button onClick={() => this.addtask()} className="button"><AddCircle className="addicon"></AddCircle></Button>
             </div>
             <div>
@@ -42,8 +55,8 @@ class Diary extends Component {
                 <div className="TODO">
                   {task}
                   <div className="todoicons">
-                    <Edit></Edit>
-                    <Cancel></Cancel>
+                  <Button onClick={() => this.setState({input: task, button: "Update", key: task})} className="button"><Edit></Edit></Button>
+                  <Button onClick={() => this.removetask(task)} className="button"><Cancel></Cancel></Button>
                   </div>
                 </div>
               ))}
